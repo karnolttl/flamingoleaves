c0c14995dd34680b6326837ea58f6f37a1b96f5c
karnolttl <karn0lttl@gmail.com>
2017-06-07 21:59:22 -0400

    image edits

--- database/factories/ImgFactory.php
+++ database/factories/ImgFactory.php
@@ -3,7 +3,7 @@
 $factory->define(App\Img::class, function (Faker\Generator $faker) {
 
     return [
-        'name' => "pattern" . rand(1,20) . ".png",
+        'name' => "pattern" . rand(1,19) . ".png",
         'owner_id' => 1,
         'post_id' => 1,
     ];
